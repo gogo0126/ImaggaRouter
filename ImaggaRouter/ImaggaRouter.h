59500dd82aa6fc53d8ba932e5df2a7ba88ee1dd9
Jerry Liao <jerryliao@anuegroup.com.tw>
2019-01-22 10:08:17 +0800

    Initial ImaggaRouter

---
+++ ImaggaRouter/ImaggaRouter.h
@@ -0,0 +1,19 @@
+//
+//  ImaggaRouter.h
+//  ImaggaRouter
+//
+//  Created by JerryLiao on 2019/1/22.
+//  Copyright © 2019 JerryLiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImaggaRouter.
+FOUNDATION_EXPORT double ImaggaRouterVersionNumber;
+
+//! Project version string for ImaggaRouter.
+FOUNDATION_EXPORT const unsigned char ImaggaRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImaggaRouter/PublicHeader.h>
+
+
